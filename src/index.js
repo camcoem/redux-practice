da0e010f486f6d9ef137471a254812f00705d4a2
camcoem <94968301+camcoem@users.noreply.github.com>
2023-01-22 23:58:20 +0100

    added documentation of pipe and compose /lodash

--- src/index.js
+++ src/index.js
@@ -6,14 +6,23 @@ let username = " Harley ";
 let message = "Hello " + username.trim() + " Good Morning!";
 
 const trim = (name) => name.trim();
-
 const generateMessage = (name) => `Hello ${name}, Good Morning!`;
-
 const convertToUpper = (name) => name.toUpperCase();
 
 // const result = generateMessage(convertToUpper(trim(username)));
 
+// compose and pipe from loadash, in order to be able to symplify the nested functions
 const newFunc = pipe(trim, convertToUpper, generateMessage);
 
+//pipe: helps to read the different functions in order of use from left to right
 const result = newFunc(username);
 console.log(result);
+
+// Currying functions: technique that allows us to take multiple arguments of functions and convert them into a function that has a simple argument
+function multi(a) {
+  return function (b) {
+    return a * b;
+  };
+}
+
+let result1 = multi(2)(5);
