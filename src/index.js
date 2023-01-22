c3e57727c13187df81c8876806397974d6c50f65
camcoem <94968301+camcoem@users.noreply.github.com>
2023-01-23 00:08:08 +0100

    added currying e.g and updated fns

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ let username = " Harley ";
 let message = "Hello " + username.trim() + " Good Morning!";
 
 const trim = (name) => name.trim();
-const generateMessage = (name) => `Hello ${name}, Good Morning!`;
+const generateMessage = (message) => (name) => `Hello ${name}, ${message}!`;
 const convertToUpper = (name) => name.toUpperCase();
 
 // const result = generateMessage(convertToUpper(trim(username)));
 
 // compose and pipe from loadash, in order to be able to symplify the nested functions
-const newFunc = pipe(trim, convertToUpper, generateMessage);
+const newFunc = pipe(trim, convertToUpper, generateMessage("Have a nice day!"));
 
 //pipe: helps to read the different functions in order of use from left to right
 const result = newFunc(username);
@@ -26,3 +26,4 @@ function multi(a) {
 }
 
 let result1 = multi(2)(5);
+const multiple = (a) => (b) => a * b; //same as above in arrow function syntax
